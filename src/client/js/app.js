347cded01a089d216fd25991609de09f1d613442
Maxim <maximzakharov@MacBook-Pro-Maxim-2.local>
2021-07-06 12:02:13 +0300

    Added short wallet address to leaderboard

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -294,14 +294,14 @@ function setupSocket(socket) {
             status += '<br />';
             if (leaderboard[i].id == player.id){
                 if(leaderboard[i].name.length !== 0)
-                    status += '<span class="me">' + (i + 1) + '. ' + leaderboard[i].name + ' - ' + leaderboard[i].address + "</span>";
+                    status += '<span class="me">' + (i + 1) + '. ' + leaderboard[i].name + ' - ' + leaderboard[i].address + '</span>';
                 else
-                    status += '<span class="me">' + (i + 1) + ". An unnamed cell</span>";
+                    status += '<span class="me">' + (i + 1) + '. An unnamed cell' + ' - ' + leaderboard[i].address + '</span>';
             } else {
                 if(leaderboard[i].name.length !== 0)
-                    status += (i + 1) + '. ' + leaderboard[i].name;
+                    status += (i + 1) + '. ' + leaderboard[i].name + ' - ' + leaderboard[i].address;
                 else
-                    status += (i + 1) + '. An unnamed cell';
+                    status += (i + 1) + '. An unnamed cell' + ' - ' + leaderboard[i].address;
             }
         }
         //status += '<br />Players: ' + data.players;
